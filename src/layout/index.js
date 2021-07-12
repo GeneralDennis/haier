40c492a0375a15b17f9dc72a0bb4e8201f3ad1e4
Скобелев Денис <generaldennis88@gmail.com>
2021-07-12 22:32:16 +0300

    #init, add header and main content

---
+++ src/layout/index.js
@@ -0,0 +1,11 @@
+import './base';
+import './grid';
+import './wrapper';
+import './content';
+import './header';
+import './footer';
+import './container';
+
+export default () => {
+  console.log('init layout');
+};
---
+++ src/pages/home/index.js
@@ -0,0 +1,5 @@
+import './index.sass';
+import './index.pug';
+import './about';
+
+export default () => {};
